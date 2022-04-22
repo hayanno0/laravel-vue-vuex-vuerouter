e659aa15b8d00357921834b17fcca39746096709
hayanno0 <hayato.miura@axia-works.com>
2022-04-23 04:09:49 +0900

    vue routerを用いた画面遷移実装

---
+++ src/resources/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <span>test</span>
+        <main>
+            <div className="container">
+                <RouterView/>
+            </div>
+        </main>
+    </div>
+</template>
